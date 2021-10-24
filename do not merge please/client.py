2d897106b0a0ff7dde4472d7205eb7b2d1c58cb1
JohnRob25 <60685571+JohnRob25@users.noreply.github.com>
2021-10-24 16:47:40 -0400

    Add files via upload
    proof of concept for sending images

--- do not merge please/client.py
+++ do not merge please/client.py
@@ -1,22 +1,35 @@
 import socket
 import base64
 import zlib
+import math
+import numpy as np
+import time
 
 def encode_image():
     with open ("donut.png", "rb") as img_file:
         ans = bytearray(img_file.read())
     print(ans)
     ans = zlib.compress(ans)
-    #ans = base64.b32encode(ans)
     print(len(ans))
     return ans
 
+def createList(img_whole):
+    size = math.ceil(len(img_whole)/250)
+    print(size)
+    fin_arr = [img_whole[i:i+250] for i in range(0, len(img_whole), 250)]
+    print(len(fin_arr))
+    return fin_arr
+
 def main():
-    len = encode_image()
+    img = encode_image()
     s = socket.socket()
     port = 12345
-    s.connect(('10.151.156.213', port))
-    s.send(len)
+    final = createList(img)
+    s.connect(('10.151.132.101', port))
+    s.send(len(final).to_bytes(100, 'big'))
+    time.sleep(1)
+    for i in range(len(final)):
+        s.send(final[i])
     s.close()
 
 main()
--- do not merge please/server.py
+++ do not merge please/server.py
@@ -1,12 +1,16 @@
 import socket
 import base64
 import zlib
+import numpy as np
 
 def decodeImage(yoshi):
 	ans = yoshi
 	#ans = base64.b32decode(yoshi)
 	with open('newImg.png', "wb") as fh:
 		fh.write(zlib.decompress(ans))
+
+def recombineImg(recimg):
+	print("ops")
 
 def main():
 	s = socket.socket()
@@ -21,16 +25,18 @@ def main():
 	while True:
 		c, addr = s.accept()
 		print("Got connection from ", addr)
-		poggers = c.recv(214748364)
-		#print(poggers.decode())
-		decodeImage(poggers)
+		poggers = c.recv(100)
+		lenOfInp = int.from_bytes(poggers, byteorder='big')
+		print(lenOfInp)
+		tempArr = [None] * lenOfInp
+		for i in range(lenOfInp):
+			nextUp = c.recv(256)
+			tempArr[i] = nextUp
+		finArr = bytearray()
+		for i in range(lenOfInp):
+			finArr += tempArr[i]
+		print(finArr)
+		decodeImage(finArr)
 		c.close()
-		if poggers != None:
-			startCounting = True
-		if startCounting:
-			i += 1
-		if poggers != None and i > 0:
-			print("\n" + str(i))
-			exit()
-
+		exit()
 main()
