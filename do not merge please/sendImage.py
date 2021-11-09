600d91f31fdb801bcd5ce73e2af51cb2cd75e0bc
JohnRob25 <60685571+JohnRob25@users.noreply.github.com>
2021-11-09 09:33:59 -0500

    Update sendImage.py

--- do not merge please/sendImage.py
+++ do not merge please/sendImage.py
@@ -6,11 +6,11 @@ def sendImage(img_name):
 	arr = encoder.encode_image(img_name)
 	arr = encoder.create_list(arr)
 	leng = len(arr)
-	incoming_img = {"INCOMING IMAGE" : leng}
+	incoming_img = {"image" : leng}
 	size_json = json.dumps(incoming_img)
 	comm.communicateData(size_json)
 	for i in range(leng):
 		nextImg = {str(arr[i]) : i}
 		next_json = json.dumps(nextImg)
 		comm.communicateData(next_json)
-sendImage("donut.png")
+# sendImage("donut.png")
