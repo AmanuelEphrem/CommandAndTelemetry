7b64986bf8d45adf951941c44749386ddef51026
JohnRob25 <60685571+JohnRob25@users.noreply.github.com>
2021-11-14 16:58:45 -0500

    Add files via upload

--- testingfolder/sendImage.py
+++ testingfolder/sendImage.py
@@ -1,27 +1,39 @@
 import json
 import encoder
-import CommunicationProtocol as comm
+from CommunicationProtocol import CommunicationProtocol 
 
 def sendImage(img_name):
+#	arr = encoder.encode_image(img_name)
+#	arr = encoder.create_list(arr)
+#	leng = len(arr)
+#	incoming_img = {"body" : "image", "size" : leng}
+#	size_json = json.dumps(incoming_img)
+#	comm.communicateData(size_json)
+#	for i in range(leng):
+#		nextImg = {str(arr[i]) : i}
+#		next_json = json.dumps(nextImg)
+#		comm.communicateData(next_json)
+	comm = CommunicationProtocol()
 	arr = encoder.encode_image(img_name)
 	arr = encoder.create_list(arr)
 	leng = len(arr)
-	incoming_img = {leng : "image"}
+	incoming_img = {"body" : "image", "size": leng}
 	size_json = json.dumps(incoming_img)
 	comm.communicateData(size_json)
 	for i in range(leng):
-		nextImg = {str(arr[i]) : i}
-		next_json = json.dumps(nextImg)
-		comm.communicateData(next_json)
+		sendMessage(arr[i], i)
 
-def receiveImage(arrSize, newimg): # Enters this function if you received the "image" message
+def sendMessage(message, numSent):
+	sendingPacket = message
+	loraRadio.send(sendingPacket, identifier = numSent)
+
+
+def receiveImage(arrSize, newimg): # Enters this function if you received the "IMAGE" message
 	imgarr = [None] * arrSize
 	for i in range(arrSize):
-		ans = comm.receiveData()
-		tempkey = list(ans.keys())
-		tempval = list(ans.values())
-		imgarr[int(tempkey[0])] = bytearray(tempval[0])
+		packet = loraRadio.recieve(with_header = True)
+		imgarr[packet[2]] = packet[4]
 	finarr = encoder.recombine_list(imgarr)
-	encoder.decode_image(finarr, newimg)
+	encoder.decode_image(finarr, "newimg.png")
 
 # sendImage("donut.png")
