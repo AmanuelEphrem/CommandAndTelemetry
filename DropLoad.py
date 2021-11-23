b5606e06a3ff19e8a07f90d8f1130a2ef7564db8
keckles <kecklesgame@gmail.com>
2021-11-23 09:28:51 -0500

    setup dropload function

---
+++ DropLoad.py
@@ -0,0 +1,19 @@
+import time
+
+import RPi.GPIO as GPIO
+
+target = 20 #change as needed
+
+# load set up
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(target,GPIO.OUT,initial=GPIO.LOW) 
+
+#drops the load
+def dropLoad:
+	GPIO.output(target,GPIO.HIGH)
+	time.sleep(.5)
+	GPIO.output(target,GPIO.LOW)
+	time.sleep(.5)
+
+#99% of this was just stolen from the beacon
