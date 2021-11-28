51cca8b17fc9314c7a75dce677944112c5ba05b7
Amanuel Ephem <amanuelephrem776@gmail.com>
2021-11-28 02:38:41 -0500

    fixed commit issue

--- CommunicationProtocol.py
+++ CommunicationProtocol.py
@@ -9,13 +9,11 @@ import board # this is found only on raspberry pi
 #import adafruit_ssd1306
 import adafruit_rfm9x #import Rfm9x
 import json
-import copy
 
 class CommunicationProtocol:
 
 	#Begins LoRa during instantiation
-	def __init__(self, subgroupDictValue:dict):
-		self.subgroupDict= subgroupDictValue
+	def __init__(self):
 		self._loraSetup()
 
 	#Setup for the LoRa module
@@ -30,13 +28,11 @@ class CommunicationProtocol:
 	#Sends JSON using LoRa
 	#@param  jsonObject  a JSON string to send
 	#@return  True if sending is successful, False otherwise
-	def _sendJSON(self,jsonObject:str) -> bool:
+	def _sendJSON(self,jsonObject) -> bool:
 		jsonObject.encode("utf-8","strict")
 		sendingPacket = bytes(jsonObject,"utf-8")
 		self.loraRadio.send(sendingPacket)
 		return True
-	
-	#Receives JSON using LoRa
 	#@return  the received JSON object as a dictionary, or None if nothing was received
 	def _receiveJSON(self) -> dict:
 		receivedPacket = self.loraRadio.receive()
@@ -46,35 +42,11 @@ class CommunicationProtocol:
 			receivedPacket.decode("utf-8","strict")
 			receivedJson = json.loads(receivedPacket)
 			return receivedJson
-	
-	#Reads raw bytes to memory in specific directory
-	def _readAndStoreImageBytes():
-		pass
 		
-	#Assigns the jsonData to the correct subgroup
-	#@param  jsonData  a json dictionary 
-	def _delegateData(self, jsonData:dict) -> bool:
-		#if key doesn't exist
-		if subgroupDict.get(jsonData["receiver"]) == None:
-			return False
+	#Attempts to send, receive, and return received json
+	#@param  jsonToSend  a JSON string to send
+	#@return  the received JSON object as a dictionary, or None if nothing was received
+	def communicateData(self, jsonToSend) -> dict:
+		self._sendJSON(jsonToSend)
+		return self._receiveJSON()
 
-		subgroupDict[jsonData["receiver"]].setPayloadData(copy.deepCopy(jsonData))	
-		return True
-
-	#Receives payload and distributes it to the correct subgroup and sends subgroup data over LoRa
-	def eventLoop(self):
-		#Receives payload
-		payload = _receiveJSON()
-		
-		#distribute payload
-		if payload != None:
-			if payload["type"] == "image":
-				_readAndStoreImageBytes()
-			else:
-				_delegateData(payload)
-
-		#send subgroupData over LoRa
-		for element in list(subgroupDict.values()):
-			sendingJSON = element.packageSubgroupData()
-			if sendingJSON != None:
-				_sendJSON(sendingJSON)
