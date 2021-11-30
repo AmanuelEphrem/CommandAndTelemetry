765ff197acd4db006c9075d5f0c464d5d2f4dd75
JohnRob25 <60685571+JohnRob25@users.noreply.github.com>
2021-11-30 01:24:31 -0500

    Create threadSocket.py
    I'm not totally sure what I actually need to have. Right now this is just code for a server that receives a packet and then puts it in the correct place AFAIK.

---
+++ src/threadSocket.py
@@ -0,0 +1,29 @@
+import socket
+import threading
+import json
+
+def thrSocket(groups: dict):
+	s = socket.socket()
+	print("Socket")
+	port = 12345
+	s.bind(('127.0.0.1', port))
+	print("Socket bound")
+	s.listen(5)
+	print("Listening")
+	i = 0
+	while True:
+		c, addr = s.accept()
+		print("Got connection from ", addr)
+		recd = c.recv(10000)
+		recd.decode("utf-8","strict")
+		recd = json.loads(recd)
+		groups[recd["sender"]].setSubGroupData(recd)
+
+# def main(): # This shouldn't be called. It's pretty much all for testing.
+# 	g = {1:2, 3:4}
+# 	t1 = threading.Thread(target = thrSocket, daemon = True, args =(g,)) # IMPORTANT: Make sure the args value is a TUPLE. Since we're using one dict, it has to be formatted like (dict,)
+# 	t1.start()
+# 	i = 0
+# 	while i < 100000:
+# 		i += 1
+# 	t1.join()
