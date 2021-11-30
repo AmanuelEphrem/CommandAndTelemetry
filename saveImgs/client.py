48b2f60458d379f0783cf922a9e0fc4c2079d9b8
JohnRob25 <60685571+JohnRob25@users.noreply.github.com>
2021-11-30 01:42:43 -0500

    Add files via upload

---
+++ saveImgs/client.py
@@ -0,0 +1,15 @@
+import socket
+import encoder
+
+def main():
+    img = encoder.encode_image("donut.png")
+    s = socket.socket()
+    port = 57762
+    final = encoder.create_list(img)
+    s.connect(('*SERVER IP*', port))
+    s.send(len(final).to_bytes(100, 'big'))
+    for i in range(len(final)):
+        s.send(final[i])
+    s.close()
+
+main()
---
+++ saveImgs/server.py
@@ -0,0 +1,26 @@
+import socket
+import threading
+import encoder
+
+def main():
+	s = socket.socket()
+	print("Socket")
+	port = 57762
+	s.bind(('*LOCAL IP*', port))
+	print("Socket bound")
+	s.listen(5)
+	print("Listening")
+	while True:
+		c, addr = s.accept()
+		print(addr)
+		print("Got connection from ", addr)
+		recd = c.recv(100)
+		lenOfInp = int.from_bytes(recd, byteorder='big')
+		print(lenOfInp)
+		tempArr = [None] * lenOfInp
+		for i in range(lenOfInp):
+			nextUp = c.recv(256)
+			tempArr[i] = nextUp
+		finArr = encoder.recombine_list(tempArr)
+		encoder.decode_image(finArr, "imgName%d.png"%addr[1])
+main()
