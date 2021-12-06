a38c93a9532fdc9997d82158af23f3b2e2273682
JohnRob25 <60685571+JohnRob25@users.noreply.github.com>
2021-12-06 16:50:59 -0500

    Update server.py

--- saveImgs/server.py
+++ saveImgs/server.py
@@ -14,13 +14,10 @@ def main():
 		c, addr = s.accept()
 		print(addr)
 		print("Got connection from ", addr)
-		recd = c.recv(100)
-		lenOfInp = int.from_bytes(recd, byteorder='big')
-		print(lenOfInp)
-		tempArr = [None] * lenOfInp
+		tempArr = []
 		for i in range(lenOfInp):
 			nextUp = c.recv(256)
-			tempArr[i] = nextUp
+			tempArr.append(nextUp)
 		finArr = encoder.recombine_list(tempArr)
 		encoder.decode_image(finArr, "imgName%d.png"%addr[1])
 if __name__ == "__main__":
