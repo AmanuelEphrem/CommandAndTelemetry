4c9813d0175e17f8a0b270deb12004c01c8d2c46
JohnRob25 <60685571+JohnRob25@users.noreply.github.com>
2021-12-06 00:32:59 -0500

    Update main.py

--- src/Ground/main.py
+++ src/Ground/main.py
@@ -4,13 +4,20 @@ import CommunicationProtocol
 import SubgroupClass
 import ThreadingClass
 import Threading
+import socket
 def main():
 	groundStation = SubgroupClass("groundStation",56783)
 	dictOfSubgroup = {"groundStation":groundStation}	
 	commProtocol = CommunicationProtocol(dictOfSubgroup)
 		
+	# This is super ugly, but it allows us to get the local ip of the pi and use that as the running IP for the socket.
+	# I don't know how to communicate this to ground station...
+	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	s.connect(('8.8.8.8', 80))
+	IP = s.getsockname()[0]
+	s.close()
 	#begin background thread (passing in groundStation as a reference)
-	x = threading.Thread(target=ThreadingClass.thrSocket,args=(dictOfSubgroups, "THIS MUST BE THE LOCAL IP OF THE MACHINE"),daemon=True)
+	x = threading.Thread(target=ThreadingClass.thrSocket,args=(dictOfSubgroups, IP),daemon=True)
 	x.start()
 	
 	keepConnectionAlive = True
